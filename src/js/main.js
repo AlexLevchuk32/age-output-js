ca1d741a23f4bd404143ff7607bd5425b94676d6
Alex Levchuk <alexlevchuk@mail.ru>
2023-08-19 18:28:32 +0300

    add age output with right words ending

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+let userAge = 101;
+
+function getAgeString(age) {
+	let count = age % 100;
+
+	if (count >= 10 && count <= 20) {
+		return 'лет';
+	} else {
+		count = age % 10;
+
+		if (count === 1) {
+			return 'год';
+		} else if (count >= 2 && count <= 4) {
+			return 'года';
+		} else {
+			return 'лет';
+		}
+	}
+}
+
+console.log(userAge + ' ' + getAgeString(userAge));
